2c327b509a3902ccb00324e60b99a369c944828c
Fran <rosasfran.97@gmail.com>
2025-01-08 15:56:44 -0300

    limpiando..

--- GestionComercio/src/main/java/com/OndaByte/GestionComercio/App.java
+++ GestionComercio/src/main/java/com/OndaByte/GestionComercio/App.java
@@ -33,30 +33,3 @@ public class App {
         app.put("/Productos/{id}/Modificar", ProductoControl::modificar); // Cambiado a PUT por ser una actualización completa
     }
 }
-/**
-import static spark.Spark.*;
-
- * App
- * @author Fran
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        before("/protegido/*", FiltroAutenticador.filtro);
-        post("/protegido/actualizar", UsuarioControl.cambiarcontra);
-        get("/protegido/usuarios", UsuarioControl.usuarios);
-        post("/protegido/eliminar", UsuarioControl.baja);
-        post("/registrar", UsuarioControl.registrar);
-        get("/login", UsuarioControl.loginForm);
-        post("/login", UsuarioControl.login);
-
-
-        get("/Productos", ProductoControl.listar);
-        post("/Productos/Alta", ProductoControl.alta);
-        post("/Productos/:id/SumarStock", ProductoControl.sumarStock);
-        delete("/Productos/:id", ProductoControl.baja);
-        post("/Productos/:id/Modificar", ProductoControl.modificar);
-    }
-}
-*/
--- GestionComercio/src/main/java/com/OndaByte/GestionComercio/control/UsuarioControl.java
+++ GestionComercio/src/main/java/com/OndaByte/GestionComercio/control/UsuarioControl.java
@@ -16,16 +16,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class UsuarioControl{
-	private static ObjectMapper objectMapper = new ObjectMapper();
+  public class UsuarioControl{
+        private static ObjectMapper objectMapper = new ObjectMapper();
 	
-	public static void usuarios (Context ctx) {
-		DAOUsuario dao = new DAOUsuario();
-        List<Usuario> usuarios = dao.listar();
-		ctx.status(200).json(usuarios);
-	}
+        public static void usuarios (Context ctx) {
+            DAOUsuario dao = new DAOUsuario();
+            List<Usuario> usuarios = dao.listar();
+            ctx.status(200).json(usuarios);
+        }
 
-	public static void login(Context ctx) {
+public static void login(Context ctx) {
 		LoginPost peticion;
 		try {
 			peticion = objectMapper.readValue(ctx.body(), LoginPost.class);
@@ -132,112 +132,3 @@ public class UsuarioControl{
     }
 	
 }
-
-
-
-/*
-  import spark.Request;
-  import spark.Response;
-  import spark.Route;
-
-  public class UsuarioControl {
-
-  private static ObjectMapper objectMapper = new ObjectMapper();
-
-  public static Route usuarios = (Request req, Response res) -> {
-  DAOUsuario dao = new DAOUsuario();
-  List<Usuario> usuarios = dao.listar();
-  res.status(200);
-  return usuarios.toString();
-  };
-
-  public static Route login = (Request req, Response res) -> {
-  LoginPost peticion = objectMapper.readValue(req.body(), LoginPost.class);
-  //ESTO TENGO QUE MOVERLO A MANEJADOR DE EXCEPCIONES/CONTROLES
-  if(peticion.getUsuario() == null || peticion.getContra() == null) {
-  res.status(400);
-  return "Usuario y Contraseña requeridos";
-  }
-
-  DAOUsuario dao = new DAOUsuario();
-  DAORol daoRol = new DAORol();
-  Usuario aux = dao.getUsuario(peticion.getUsuario());
-
-  if (aux != null && BCrypt.checkpw(peticion.getContra(), aux.getContra())){
-  res.body("{ \"token\" : \""+Seguridad.getToken(aux.getUsuario())+"\", \"permisos\" : " + daoRol.getPermisosUsuario(aux.getId()) +"}\n");
-  res.status(200);
-  return res.body();
-  }
-  else{
-  res.status(500);
-  return "Error al loguear";
-  }
-  };
-
-  public static Route cambiarcontra = (Request req, Response res) -> {
-  DAOUsuario dao = new DAOUsuario();
-  String usuario = req.queryParams("usuario");
-  String contra = req.queryParams("contra");
-  String nueva = req.queryParams("nueva");
-
-  //ESTO TENGO QUE MOVERLO A MANEJADOR DE EXCEPCIONES/CONTROLES
-  if(usuario == null || contra == null) {
-  res.status(400);
-  return "Usuario y Contraseña requeridos";
-  }
-
-  Usuario aux = dao.getUsuario(usuario);
-
-  if (BCrypt.checkpw(contra, aux.getContra())){
-  aux.setContra(BCrypt.hashpw(nueva, BCrypt.gensalt()));
-  if(dao.modificar(aux)){
-  res.status(201);
-  return "Contraseña actualizada";
-  }
-  else{
-  res.status(404);
-  return "ERROR: No se pudo actualizar la contraseña";
-  }
-  }
-  else{
-  res.status(500);
-  return "Error al loguear";
-  }
-  };
-
-  public static Route registrar = (Request req, Response res) -> {
-  DAOUsuario dao = new DAOUsuario();
-  LoginPost peticion = objectMapper.readValue(req.body(), LoginPost.class);
-  //ESTO TENGO QUE MOVERLO A MANEJADOR DE EXCEPCIONES/CONTROLES
-  if(peticion.getUsuario() == null || peticion.getContra() == null) {
-  res.status(400);
-  return "Usuario y Contraseña requeridos";
-  }
-
-  Usuario nuevo = new Usuario();
-  nuevo.setUsuario(peticion.getUsuario());
-  nuevo.setContra(BCrypt.hashpw( peticion.getContra(), BCrypt.gensalt()));
-
-  if(dao.alta(nuevo)){
-  res.status(201);
-  return "Registro exitoso";
-  }
-  else{
-  res.status(500);
-  return "Error al registrar";
-  }
-  };
-
-  public static Route loginForm = (Request req, Response res) -> {
-  return "no implementado sory";
-  };
-
-  public static Route baja = (Request req, Response res) -> {
-  String id = req.queryParams("id");
-  String borrar = req.queryParams("borrar");
-  DAOUsuario dao = new DAOUsuario();
-  return dao.baja(id,Boolean.valueOf(borrar));
-  };
-  }
-
-*/
